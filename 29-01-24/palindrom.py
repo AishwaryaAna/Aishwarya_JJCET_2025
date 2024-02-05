6cda537b1b02f81ad4b01ce0b9696ec6fe3ec919
AishwaryaAna <146980016+AishwaryaAna@users.noreply.github.com>
2024-02-05 20:49:42 +0530

    Create palindrom.py

---
+++ 29-01-24/palindrom.py
@@ -0,0 +1,18 @@
+'''
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+'''
+class Solution:
+  def isPalindrome(self, x: int) -> bool:
+    if x < 0:
+      return False
+
+    rev = 0
+    y = x
+
+    while y:
+      rev = rev * 10 + y % 10
+      y //= 10
+
+    return rev == x 
